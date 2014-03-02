ecaa05ca30b844d208aa523f708faff0810e22b3
[Izz Abudaka] <[izz.abudaka@student.manchester.ac.uk]>
2014-03-02 14:15:08 +0000

    modified index, supposed to be clickable

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <title>PLOP</title>
-  
+
+
   <!-- DROPZONE -->
   <link href="css/dropzone.css" type="text/css" rel="stylesheet" />
   <script src="dropzone.min.js"></script>
+  <!-- Add fancyBox -->
+  <link rel="stylesheet" href="/fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+  <script type="text/javascript" src="/fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
     
   <!-- SHOWING ALL FILES USING JQUERY -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
@@ -19,6 +23,7 @@
         var mockFile = { name: value.name, size: value.size };
         thisDropzone.options.addedfile.call(thisDropzone, mockFile);
         thisDropzone.options.thumbnail.call(thisDropzone, mockFile, "uploads/"+value.name);
+        thisDropzone.clickable = true;
       }); 
     });
   }
