78a7d1ea2f0c3e54740bbb5d0522ac9bde63f8b3
darkelink <darkelink@gmail.com>
2014-03-11 03:23:45 +0000

    added auto naming, sorting and deletion of uploaded files

--- upload.php
+++ upload.php
@@ -9,14 +9,33 @@ $storeFolder = 'uploads';
 if (!empty($_FILES)) {
   // A file was uploaded
 
+  // Get info about the file
   $size = $_FILES['file']['size'];
   $tempFile = $_FILES['file']['tmp_name'];
   $mimeType = getimagesize($tempFile)['mime'];
 
+  // Check if valid file TODO:send error if not
   if (in_array($mimeType, array_merge(unserialize(IMAGE_FILE_TYPES))) && $size < MAX_FILE_SIZE) {
     $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;
-    $targetFile =  $targetPath. $_FILES['file']['name']; 
+
+    // Scan the directory for the next missing file name
+    $nextName = -1;
+    $itr = 0;
+    while ($nextName == -1) {
+      if (!file_exists($targetPath. $itr++)) {
+        $nextName = $itr - 1;
+      }
+    }
+    // Move the uploaded file and use the empty file name that was found
+    $targetFile =  $targetPath. $nextName; 
     move_uploaded_file($tempFile,$targetFile);
+  
+    // Check we the max number of files hasn't been reached
+    if ($itr == MAX_FILES) {
+      $itr = 0;
+    }
+    // Delete the next file, leaving a new space
+    unlink($targetPath. $itr);
   }
 } else { 
   // Opening the page
