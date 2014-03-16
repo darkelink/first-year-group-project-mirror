b3077943c10ae6961644d01af3bcc6b31b463ccd
Catalin <catalin.terzea@student.manchester.ac.uk>
2014-03-16 15:10:31 +0000

    Kappa

--- report.php
+++ report.php
@@ -4,13 +4,12 @@ require_once("config.inc.php");
 require_once("constants.php");
 
 $ClientIP = $_SERVER['REMOTE_ADDR'];
-$file_id = $_POST['file']['name'];
+$file_id = 0;//$_POST['file']['name'];
 $found = false;
 
 $mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
 
 // Check Database for Client IP and Client Proxy
-echo "This always works\n";
 if($stmt = $mysqli->prepare("SELECT `Client IP` FROM `IP_Addresses`"))
 {
   echo "After first stmt\n";
@@ -21,21 +20,20 @@ if($stmt = $mysqli->prepare("SELECT `Client IP` FROM `IP_Addresses`"))
   {
     // CHECK LIST FOR IP
     echo "Test 1 - before ip check\n";
-    echo "$found\n";
     echo "IP in datab : $ip\n";
     if($ip == $ClientIP && !$found)
     {
       $found = true;
-      echo "Test 2 - after ip check\n";
-  $stmt->close();
+      echo "Test 2 - after ip found\n";
+      $stmt->close();
       if($r_stmt = $mysqli->prepare("SELECT `Reports` FROM `IP_Addresses` WHERE `Client IP` = ?"))
       {
         echo "First if test\n";
         $r_stmt->bind_param('s', $ip);
         $r_stmt->execute();
         $r_stmt->bind_result($report_number);
-        echo "Test report number before fetch : $report_number\n";
         $r_stmt->fetch();
+        $r_stmt->close();
         echo "Test report number : $report_number\n";
         if($report_number < MAX_REPORTS)
         {
@@ -44,26 +42,28 @@ if($stmt = $mysqli->prepare("SELECT `Client IP` FROM `IP_Addresses`"))
             echo "Check if update clause is good\n";
             $u_stmt->bind_param('is', ($report_number + 1), $ip);
             $u_stmt->execute();
+            $u_stmt->close();
           }
           if($u_stmt = $mysqli->prepare("SELECT `Reports` FROM `plop_files` WHERE `ID`= ?"))
           {
+            echo "Check if file report select works\n";
             $u_stmt->bind_param('s', $file_id);
             $u_stmt->execute();
             $u_stmt->bind_result($times_reported);
             $u_stmt->fetch();
+            $u_stmt->close();
           }
           if($u_stmt = $mysqli->prepare("UPDATE `plop_files` SET `Reports`= ? WHERE `ID`= ?"))
           {
+            echo "Check if plop_files update - Shouldn't work\n";
             $u_stmt->bind_param('is', $times_reported, $file_id);
             $u_stmt->execute();
-          }
-          if ($u_stmt != null)
-          {
             $u_stmt->close();
           }
         }
-        $r_stmt->close();
-      } else {
+      } 
+      else 
+      {
         echo $mysqli->error . '\n';
       }
     }
@@ -72,12 +72,14 @@ if($stmt = $mysqli->prepare("SELECT `Client IP` FROM `IP_Addresses`"))
   echo "Test before !found if\n";
   if (!$found)
   {
+    $stmt->close();
     echo "Test inside !found if\n";
     if($u_stmt = $mysqli->prepare("INSERT INTO `IP_Addresses`(`Client IP`, `Reports`) VALUES (?,?)"))
     {
       $temp = 1;
       $u_stmt->bind_param('si', $ClientIP, $temp);
       $u_stmt->execute();
+      $u_stmt->close();
     }
     if($u_stmt = $mysqli->prepare("SELECT `Reports` FROM `plop_files` WHERE `ID`= ?"))
     {
@@ -85,14 +87,12 @@ if($stmt = $mysqli->prepare("SELECT `Client IP` FROM `IP_Addresses`"))
       $u_stmt->execute();
       $u_stmt->bind_result($times_reported);
       $u_stmt->fetch();
+      $u_stmt->close();
     }
     if($u_stmt = $mysqli->prepare("UPDATE `plop_files` SET `Reports`= ? WHERE `ID`= ?"))
     {
       $u_stmt->bind_param('is', $times_reported, $file_id);
       $u_stmt->execute();
-    }
-    if ($u_stmt != null)
-    {
       $u_stmt->close();
     }
   }
