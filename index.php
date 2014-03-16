a97eca33b4b7d0001ee28f32c5429f0c90f68549
John Gresty <darkelink@gmail.com>
2014-03-16 06:22:18 +0000

    webgl background, fixed stylesheet, using min js, removed navbar, various other small changes

--- index.php
+++ index.php
@@ -1,27 +1,150 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="UTF-8">
   <title>PLOP</title>
+  <meta charset="UTF-8" name="vieport" 
+    content="width=device-width, user-scalable=no, minimum=scale=1.0, maximum=scale=1.0">
 
   <!-- DEFAULT -->
-  <!-- FOR SOME REASON, THIS DOESN'T WORK -->
-  <link href="css/global_style.css" type="text/css" rel="styelsheet" />
+  <link href="css/global_style.css" type="text/css" rel="stylesheet" />
 
   <!-- DROPZONE -->
   <link href="css/dropzone.css" type="text/css" rel="stylesheet" />
-  <script type="text/javascript" src="js/dropzone.js"></script>
+  <script type="text/javascript" src="js/dropzone.min.js"></script>
 
   <!-- JQUERY -->
-  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+  <script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
 
   <!-- FANCYBOX -->
-  <link rel="stylesheet" href="css/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
-  <script type="text/javascript" src="js/jquery.fancybox.js?v=2.1.5"></script>
+  <link rel="stylesheet" href="css/jquery.fancybox.css" type="text/css" media="screen" />
+  <script type="text/javascript" src="js/jquery.fancybox.pack.js"></script>
     
-  <!-- INIT PAGE -->
-  <script>
-  
+  <!-- THREE.JS -->
+  <script src="js/three.min.js"></script>
+
+  <!-- SHADERS -->
+  <script src="js/Mirror.js"></script>
+  <script src="js/WaterShader.js"></script>
+
+</head>
+
+<body>
+<script>
+  // main div
+  var container, myDropzone;
+
+  // three.js elements
+  var camera, scene, renderer;
+  var waterNormals;
+
+  // constants
+  var WIDTH = 2000;
+  var HEIGHT = 2000;
+
+
+  init();
+  animate();
+
+  // Create three.js scene
+  function init() {
+    // create all elements
+    container = document.createElement('div');
+    container.setAttribute('id', 'dropzone');
+    document.body.appendChild(container);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.5, 3000000);
+
+    // give a nice position to the camera
+    camera.position.set(0, Math.max(WIDTH * 1.5, HEIGHT) / 8, HEIGHT);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    // add a light
+    directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(- 1, 0.5, - 1);
+    scene.add(directionalLight);
+
+    // load shaders
+    waterNormals = new THREE.ImageUtils.loadTexture('images/normals.png');
+    waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
+
+    water = new THREE.Water(renderer, camera, scene, {
+      textureWidth: 512,
+      textureHeight: 512,
+      waterNormals: waterNormals,
+      alpha:  1.0,
+      sunDirection: directionalLight.position.normalize(),
+      sunColor: 0xffffff,
+      waterColor: 0x0022ff,
+      distortionScale: 50.0,
+    });
+
+    mirrorMesh = new THREE.Mesh(new THREE.PlaneGeometry(WIDTH * 500, HEIGHT * 500, 50, 50), water.material);
+
+    mirrorMesh.add(water);
+    mirrorMesh.rotation.x = -Math.PI * 0.5;
+    scene.add(mirrorMesh);
+
+    // load sky
+    var cubeMap = new THREE.Texture([]);
+    cubeMap.format = THREE.RGBFormat;
+    cubeMap.flipY = false;
+
+    var loader = new THREE.ImageLoader();
+    loader.load('images/skybox.png', function (image) {
+      // the skybox is in cubemap format, so we need each side of the cube
+      var getSide = function (x, y) {
+        var size = 1024;
+        var canvas = document.createElement('canvas');
+        canvas.width = size;
+        canvas.height = size;
+        var context = canvas.getContext('2d');
+        context.drawImage(image, - x * size, - y * size);
+        return canvas;
+      };
+
+      cubeMap.image[0] = getSide(2, 1);
+      cubeMap.image[1] = getSide(0, 1);
+      cubeMap.image[2] = getSide(1, 0);
+      cubeMap.image[3] = getSide(1, 2);
+      cubeMap.image[4] = getSide(1, 1);
+      cubeMap.image[5] = getSide(3, 1);
+      cubeMap.needsUpdate = true;
+    });
+
+    // add sky
+    var cubeShader = THREE.ShaderLib['cube'];
+    cubeShader.uniforms['tCube'].value = cubeMap;
+
+    var skyBoxMaterial = new THREE.ShaderMaterial({
+      fragmentShader: cubeShader.fragmentShader,
+      vertexShader: cubeShader.vertexShader,
+      uniforms: cubeShader.uniforms,
+      depthWrite: false,
+      side: THREE.BackSide
+    });
+
+    var skyBox = new THREE.Mesh(new THREE.BoxGeometry(1000000, 1000000, 1000000), skyBoxMaterial);
+    scene.add(skyBox);
+  }
+
+  // animate three.js scene
+  function animate() {
+    requestAnimationFrame(animate);
+    render();
+  }
+  // render three.js scene
+  function render() {
+    water.material.uniforms.time.value += 1.0 / 60.0;
+    water.render();
+    renderer.render(scene, camera);
+  }
+
+
   // display all files in the dropzone
   function getFiles(dz) {
     $.getJSON('upload.php', function(data) {
@@ -37,29 +160,30 @@
     });
   }
 
-  // setup Dropzone
-  Dropzone.options.myDropzone = {
+  // setup Dropzone (must be done after three.js)
+  myDropzone = new Dropzone("div#dropzone", { 
+    url: "upload.php", 
     maxFileSize: 20,
     uploadMultiple: false,
     parallelUploads: 1,
     clickable: true,
     acceptedFiles: "image/*",
     previewTemplate: "<div class='dz-preview dz-file-preview'><div class='dz-details'><img data-dz-thumbnail /></div><div class='dz-progress'><span class='dz-upload' data-dz-uploadprogress></span></div><div class='dz-success-mark'><span></span></div><div class='dz-error-mark'><span></span></div><div class='dz-error-message'><span data-dz-errormessage></span></div></div>",
-      
+
     // add all files from uploads/ to dropzone when the page loads
     init: function() {
       getFiles(this);
     }
-  };
+  });
 
-      
-  // Setup fancybox
+  // Setup fancybox (jquery)
   $(document).ready(function() {
     $(".dz-preview").fancybox();
 
     $(".dz-preview").fancybox({
       transitionIn : "elastic",
       transitionOut : "elastic",
+      padding: 0,
       speedIn : 600, 
       speedOut : 200,
       // Add report button to the end
@@ -85,20 +209,11 @@
        }
      });   
   }
-  </script>
-</head>
 
-<body>	
+  </script>
+
     
-  <form action="upload.php" class="dropzone" id="my-dropzone">
-    <div class="fallback">
-      <span>Please update your browser to properly use this site.</span>
-    </div>
-  </form>
-    
-  <div class = "navigation_bar">
-    <!-- must change images and links when possible -->
-    <!-- search  -->
+   <!-- this is not nessisary and also does not work without styles
     <img src="images/my_profile_unpressed.svg"
       draggable="true" ondragstart="drag(event)"
       width="60" height="60" style="margin-bottom:5px;"
@@ -107,14 +222,12 @@
       onmouseout="src='images/my_profile_unpressed.svg'"
       onCLick="search()"                                />
     </br>
-    <!-- upload  -->
     <img src="images/my_profile_unpressed.svg"
       width="60" height="60" style="margin-bottom:5px;"
       onmouseover="src='images/my_profile_pressed.svg'"
       onmousedown="src='images/my_profile_pressed.svg'"
       onmouseout="src='images/my_profile_unpressed.svg'"/>
     </br>
-    <!-- help  -->
     <img src="images/my_profile_unpressed.svg"
       width="60" height="60" style="margin-bottom:5px;"
       onmouseover="src='images/my_profile_pressed.svg'"
@@ -122,6 +235,7 @@
       onmouseout="src='images/my_profile_unpressed.svg'"/>
     </b>
   </div>
+  -->
     
 </body>
 </html>
